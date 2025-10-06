2c2474b043d32cd55c12060fd0c90e8a0ff31bdc
Adrian Castellanos <adrian.castellanos@feverup.com>
2025-10-06 22:58:21 +0200

    feat(event): admin log cleanup

--- events/admin/event.py
+++ events/admin/event.py
@@ -8,19 +8,13 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ["name", "start_date", "end_date"]
     list_filter = ["name"]
     search_fields = ["name"]
-    readonly_fields = ["image_tag"]
+    readonly_fields = ["image_preview"]
 
-    def image_tag(self, obj):
-        print("DEBUG")
-        print(obj)
-        print(obj.image)
-        print(obj.image.url)
-        print("END DEBUG")
-
+    def image_preview(self, obj):
         if obj.image:
             return format_html('<img src="{}" style="max-height: 100px;"/>', obj.image.url)
         return "-"
 
-    image_tag.short_description = "Image tag"
+    image_tag.short_description = "Image preview"
 
 admin.site.register(Event, EventAdmin)
