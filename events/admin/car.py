9b6e2677544753c6bd37699ee13c5805e24ed69c
antoniahp <a.herrerapuertas@gmail.com>
2025-10-06 21:06:08 +0200

    fix: admins

--- events/admin/car.py
+++ events/admin/car.py
@@ -5,7 +5,7 @@ from events.domain.car import Car
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ["brand", "model", "competitor__name"]
-    list_filter = ["competitor"]
+    list_filter = ["competitor__name"]
 
     def competitor_name(self, obj):
         return obj.competitor.name
--- events/admin/competitor.py
+++ events/admin/competitor.py
@@ -6,11 +6,13 @@ from events.domain.competitor import Competitor
 
 class CarInline(admin.TabularInline):
     model = Car
+    extra = 0
     fields = ['brand', 'model', 'group']
 
 
 class CompetitorAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    search_fields = ["name"]
     inlines = [CarInline]
 
 admin.site.register(Competitor, CompetitorAdmin)
