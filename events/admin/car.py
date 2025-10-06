3016207781ec4710378de7a12ef4fa8f7a6929c5
antoniahp <a.herrerapuertas@gmail.com>
2025-10-06 21:00:31 +0200

    feat: added car model

---
+++ events/admin/car.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from events.domain.car import Car
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["brand", "model", "competitor__name"]
+    list_filter = ["competitor"]
+
+    def competitor_name(self, obj):
+        return obj.competitor.name
+
+    competitor_name.admin_order_field = 'competitor__name'
+    competitor_name.short_description = 'Driver'
+
+admin.site.register(Car, CarAdmin)
