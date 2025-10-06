8a8f507f7f56ce11794ab6b55ca68383ae6d3fe8
antoniahp <a.herrerapuertas@gmail.com>
2025-10-06 21:00:58 +0200

    feat: added migrations

--- events/models/__init__.py
+++ events/models/__init__.py
@@ -1,3 +1,7 @@
+from events.domain.car import Car
+from events.domain.competitor import Competitor
 from events.domain.event import Event
+from events.domain.team import Team
+__all__ = ["Event", "Competitor", "Car", "Team"]
 
-__all__ = ["Event",]
+
